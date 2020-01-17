cffa6de75525c12bd7f22760e46ef84ba636d32f
Shimoji Reo <e195758@ie.u-ryukyu.ac.jp>
2020-01-17 21:41:14 +0900

    step2

---
+++ src/jp/ac/uryukyu/ie/e195758/Board.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e195758;
+
+public class Board {
+
+    void Board(){
+       char[][] board = {{'|','|','|','|'},{'|','|','|','|'}};
+        System.out.println(board[0]);
+        System.out.println(board[1]);
+
+
+    }
+}
--- src/jp/ac/uryukyu/ie/e195758/Piece.java
+++ src/jp/ac/uryukyu/ie/e195758/Piece.java
@@ -1,4 +1,5 @@
 package jp.ac.uryukyu.ie.e195758;
 
 public class Piece {
+
 }
