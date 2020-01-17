c92cb77649eeb41b231dc57156073f349097c9fc
Shimoji Reo <e195758@ie.u-ryukyu.ac.jp>
2020-01-17 22:43:53 +0900

    step3

--- src/jp/ac/uryukyu/ie/e195758/Board.java
+++ src/jp/ac/uryukyu/ie/e195758/Board.java
@@ -3,10 +3,9 @@ package jp.ac.uryukyu.ie.e195758;
 public class Board {
 
     void Board(){
-       char[][] board = {{'|','|','|','|'},{'|','|','|','|'}};
-        System.out.println(board[0]);
-        System.out.println(board[1]);
-
-
+       char[][] board = {{'|','銀','|','王','|','金','|'},{'|','　','|','歩','|','　','|'},{'|','　','|','歩','|','　','|'},{'|','銀','|','王','|','金','|'}};
+        for(int i = 0; i < 4; i++){
+            System.out.println(board[i]);
+        }
     }
 }
